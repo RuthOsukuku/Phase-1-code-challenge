670f42a565e471a3131667323cd0a051eeeedca5
Ruth <osukukuruth@gmail.com>
2023-05-01 15:15:45 -0400

    doing assignment

--- index.js
+++ index.js
@@ -26,3 +26,31 @@ const fetchQuotes = () => {
       });
   };
   
+  // Generate a random quote on click of a button
+function generateRandomQuote() {
+    quoteEl = document.getElementById('quote');
+    quoteAuthorEl = document.getElementById('author');
+  
+    document.getElementById('new-quote').addEventListener('click', function(e) {
+      fetch('https://type.fit/api/quotes')
+        .then(response => response.json())
+        .then(data => {
+          const randomQuoteIndex = Math.floor(Math.random() * data.length);
+          const quoteText = data[randomQuoteIndex].text;
+          let author = data[randomQuoteIndex].author;
+  
+          if (!author) {
+            author = 'Unknown';
+          }
+  
+          if (quoteText.length > 120) {
+            quoteEl.classList.add('long-quote');
+          } else {
+            quoteEl.classList.remove('long-quote');
+          }
+          quoteEl.textContent = quoteText;
+          quoteAuthorEl.textContent = author;
+        });
+    });
+  }
+  
