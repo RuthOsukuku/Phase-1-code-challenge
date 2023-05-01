3ec370bb6976825c4e81994a37577a5baae77143
Ruth <osukukuruth@gmail.com>
2023-05-01 15:13:44 -0400

    doing assignment

--- index.js
+++ index.js
@@ -7,4 +7,22 @@ document.addEventListener('DOMContentLoaded', function() {
     viewQuotes();
     deleteQuote();
   });
+// Use fetch request to get all the quotes
+const fetchQuotes = () => {
+    fetch('https://type.fit/api/quotes')
+      .then(res => res.json())
+      .then(data => {
+        data.forEach(quoteObject => {
+          const quoteList = document.getElementById('quote-list');
+          const quoteEl = document.createElement('li');
+          const spanAuthor = document.createElement('p');
+  
+          quoteEl.innerText = `${quoteObject.text}`;
+          spanAuthor.innerText = `${quoteObject.author}`;
+          spanAuthor.className = 'quote-span';
+          quoteEl.appendChild(spanAuthor);
+          quoteList.appendChild(quoteEl);
+        });
+      });
+  };
   
