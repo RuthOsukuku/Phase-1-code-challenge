380b8501dbf7ad5d497916c74a94b328c13debb2
Ruth <osukukuruth@gmail.com>
2023-05-01 15:11:40 -0400

    doing assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+document.addEventListener('DOMContentLoaded', function() {
+    fetchQuotes();
+    generateRandomQuote();
+    tweetQuote();
+    addQuote(quote);
+    createNewQuotes();
+    viewQuotes();
+    deleteQuote();
+  });
+  
